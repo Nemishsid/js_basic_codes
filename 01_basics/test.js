83f957dec5c62242b9314e2b4262de2201e9a9ba
Nemishsid <126538728+Nemishsid@users.noreply.github.com>
2024-05-30 15:40:01 +0000

    new modified file

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
-console.log("hey bro")
+console.log("hey bro");
+const accountId = 123456789;
+let accountEmail = "nemish1@gmail.com";
+var accountPassword = "121";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword]);
