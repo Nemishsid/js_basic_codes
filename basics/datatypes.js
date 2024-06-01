a8df19e0d8c488657fd758d2b3c7f78befe767c8
Nemishsid <126538728+Nemishsid@users.noreply.github.com>
2024-06-01 09:00:08 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict";
+// treate all js code as new version.
+// alert(3+3);
+/* alert like commands will use in web browser`s console
+   because they`re embedded inside an document.
+   but here they can`t show any kinds of pop-up  */
+console.log(3+3);
+console.log("Nemish");
+
+let name = "Nemish";
+let age = 22;
+let age2 = "22";
+let isLoggedIn = true;
+let state;
+
+//number ==> 2 to the power 53
+//bigint
+//string""
+//boolean ==> true/false
+//null ==> standalone value
+//undefined ==>
+//symbol ==> to find uniqueness
+//object
+// all of these were Primitive data types
+
+console.log(typeof "Nemish");
+console.log(typeof null);
+console.log(typeof undefined);
+
